61414712c05ba91acf91c19432a37c90515b811f
thienth32 <64114903+thienth32@users.noreply.github.com>
2022-01-21 14:54:10 +0700

    Update index.php

--- asm1/index.php
+++ asm1/index.php
@@ -3,13 +3,37 @@
 $url = isset($_GET['url']) ? $_GET['url'] : "/";
 // $url mong muốn của người gửi request
 switch ($url) {
-    case '/':
-        require_once './controllers/HomeController.php';
-        home_index();
+    case 'login':
         break;
-    case 'admin':
-        require_once './controllers/AdminController.php';
-        admin_index();
+    case 'dashboard':
+        break;
+    case 'mon-hoc':
+        break;
+    case 'mon-hoc/tao-moi':
+        break;
+    case 'mon-hoc/luu-tao-moi':
+        break;
+    case 'mon-hoc/cap-nhat':
+        break;
+    case 'mon-hoc/luu-cap-nhat':
+        break;
+    case 'mon-hoc/xoa':
+        break;
+    case 'mon-hoc/chi-tiet':
+        break;
+    case 'quiz':
+        break;
+    case 'quiz/tao-moi':
+        break;
+    case 'quiz/luu-tao-moi':
+        break;
+    case 'quiz/cap-nhat':
+        break;
+    case 'quiz/luu-cap-nhat':
+        break;
+    case 'quiz/xoa':
+        break;
+    case 'quiz/lam-bai':
         break;
     default:
         echo "Đường dẫn bạn đang truy cập chưa được cho phép";
